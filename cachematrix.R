4220f97ff82b4bd334380861114cef6ddc39bb74
M3rouf-B <107923963+M3rouf-B@users.noreply.github.com>
2022-07-22 11:59:43 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,67 @@
 ## functions do
 
 ## Write a short comment describing this function
+##makeCacheMatrix computes the inverse of input matrix and save it in cache
+makeCacheMatrix <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) m <<- solve
+        getinv <- function() m
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
+}
 
-makeCacheMatrix <- function(x = matrix()) {
-
+#cachesolve checks the cache for the matrix's inverse first before computes it
+cacheSolve <- function(x, ...) {
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
 }
 
 
-## Write a short comment describing this function
+##test both macros
+c=rbind(c(1, -0.3), c(-0.3, 1))
+tstm=makeCacheMatrix(c)
+cacheSolve(tstm)
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+rm(list = ls())  # remove any currently existing objects in the current R session
+
+# define makeVector and cachemean
+
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
+#
+#
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
 }
